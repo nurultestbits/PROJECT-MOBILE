3b917aed8b2175382c75ffd200c6de6a28e82dec
Nurul Izzanni <nurul.qawork@gmail.com>
2022-12-20 14:55:05 +0800

    Initial commit

---
+++ Scripts/Create Note/TCCreate1-text note/Script1671486062892.groovy
@@ -0,0 +1,41 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startExistingApplication('com.socialnmobile.dictapps.notepad.color.note')
+
+Mobile.tap(findTestObject('Object Repository/Create Note/TCCreate1/android.widget.ImageButton'), 1)
+
+Mobile.tap(findTestObject('Object Repository/Create Note/TCCreate1/android.widget.TextView - Text'), 1)
+
+Mobile.tap(findTestObject('Object Repository/Create Note/TCCreate1/android.widget.EditText'), 0)
+
+Mobile.sendKeys(findTestObject('Create Note/TCCreate1/android.widget.EditText'), 'Assalamualaikum...apa khabar?')
+
+Mobile.pressBack()
+
+Mobile.pressBack()
+
+Mobile.pressBack()
+
+Mobile.verifyElementExist(findTestObject('Create Note/TCCreate1/android.widget.LinearLayout'), 1)
+
+Mobile.pressBack()
+
+Mobile.closeApplication()
+
---
+++ Test Suites/TCSearch1(Data driven).ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TCSearch1(Data driven)</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2023730e-aea4-417b-8674-a8a9f64e938c</testSuiteGuid>
+   <testCaseLink>
+      <guid>98c29f14-17cc-44bf-8f29-da51c720d812</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Search for Words/TCSearch1-search word</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>af8b1a2f-d553-4ad6-ae59-ae3360c22675</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Data Driven- Search keyword</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>af8b1a2f-d553-4ad6-ae59-ae3360c22675</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>words</value>
+         <variableId>8018a9da-65d4-47a2-839e-6dcf47d79e1d</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
